dc9731b6471153550d7732dea32d61aa89a9c0e9
LogicMaster <traderrafa02@gmail.com>
2025-02-25 14:12:57 -0300

    Delete comandas directory

--- comandas/src/components/MesaInfo.js
+++
@@ -1,6 +0,0 @@
-export function MesaInfo() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const mesa = urlParams.get("mesa") || "N/A";
-
-  return `<h1>Cardápio - Mesa ${mesa}</h1>`;
-}
