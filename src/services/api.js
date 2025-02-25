3aba0bcea63e98c485d46ee11d6bc3fd3504f580
LogicMaster <traderrafa02@gmail.com>
2025-02-25 15:04:27 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,34 @@
-export async function enviarPedido(mesa, itens) {
-  const response = await fetch("https://seuservidor.com/pedido", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ mesa, itens }),
-  });
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-  if (response.ok) {
+const firebaseConfig = {
+  apiKey: "AIzaSyAto25h5ZeIJ6GPlIsyuXAdc4igrgMgzhk",
+  authDomain: "bar-do-cesar.firebaseapp.com",
+  databaseURL: "https://bar-do-cesar-default-rtdb.firebaseio.com",
+  projectId: "bar-do-cesar",
+  storageBucket: "bar-do-cesar.firebasestorage.app",
+  messagingSenderId: "525946263891",
+  appId: "1:525946263891:web:6179063c88e3f45d2c29a6",
+  measurementId: "G-7SZT212JXN",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export async function enviarPedido(mesa, itens) {
+  try {
+    await push(ref(db, "pedidos"), {
+      mesa: mesa,
+      itens: itens,
+      status: "aguardando",
+      timestamp: new Date().toISOString(),
+    });
     alert("Pedido enviado para a cozinha!");
-  } else {
-    alert("Erro ao enviar o pedido.");
+  } catch (error) {
+    alert("Erro ao enviar o pedido: " + error.message);
   }
 }
