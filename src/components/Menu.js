857ecc5f86f071f97da45817043d8759c0e2d1e3
LogicMaster <traderrafa02@gmail.com>
2025-03-02 16:51:28 -0300

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,15 +8,17 @@ export class Menu {
     return this.itens
       .map(
         (item) => `
-                    <div class="menu-item">
-                        <span>${item.nome} - R$ ${item.preco.toFixed(2)}</span>
-                        <button onclick="(${this.adicionarCallback})('${
-          item.nome
-        }')">
-                            Adicionar
-                        </button>
-                    </div>
-                `
+          <div class="menu-item">
+            <span>${item.nome} - R$ ${item.preco.toFixed(2)}</span>
+            <button class="adicionar-btn" data-nome="${item.nome}">
+              Adicionar
+            </button>
+            <div class="quantidade-container" style="display: none;">
+              <input type="number" class="quantidade-input" min="1" value="1" style="width: 50px;">
+              <button class="confirmar-btn">OK</button>
+            </div>
+          </div>
+        `
       )
       .join("");
   }
