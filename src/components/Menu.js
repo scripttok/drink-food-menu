50988413d36fd2a7efe78a1c4733bd8dfdde65c2
LogicMaster <traderrafa02@gmail.com>
2025-03-04 17:10:50 -0300

    upMen

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,15 +7,13 @@ export class Menu {
 
   render() {
     return this.itens
-      .map((item) => {
+      .map((item, index) => {
         console.log("Item sendo renderizado:", item);
-        const imagensJson = encodeURIComponent(JSON.stringify(item.imagens || []));
-        console.log("String JSON escapada para carrossel:", imagensJson);
         return `
           <div class="menu-item">
             ${
               item.imagens && item.imagens.length > 0
-                ? `<img src="${item.imagens[0]}" alt="${item.nome}" class="menu-item-image" onclick="abrirCarrossel(decodeURIComponent('${imagensJson}'))">`
+                ? `<img src="${item.imagens[0]}" alt="${item.nome}" class="menu-item-image" onclick="abrirCarrossel(${index})">`
                 : `<div class="menu-item-no-image">Sem Imagem</div>`
             }
             <span>${item.nome} - R$ ${(item.precoUnitario || 0).toFixed(2)}</span>
