07a1452129e573c43acd1582e9b9e024aba58540
LogicMaster <traderrafa02@gmail.com>
2025-03-04 16:18:32 -0300

    menu upe Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@ export class Menu {
   constructor(itens, adicionarCallback, abrirCarrosselCallback) {
     this.itens = itens;
     this.adicionarCallback = adicionarCallback;
-    this.abrirCarrosselCallback = abrirCarrosselCallback; // Callback para abrir o carrossel
+    this.abrirCarrosselCallback = abrirCarrosselCallback; // Novo callback para o carrossel
   }
 
   render() {
@@ -12,12 +12,10 @@ export class Menu {
           <div class="menu-item">
             ${
               item.imagens && item.imagens.length > 0
-                ? `<img src="${item.imagens[0]}" alt="${item.nome}" class="menu-item-image" onclick="(${this.abrirCarrosselCallback})('${JSON.stringify(
-                    item.imagens
-                  )}')">`
-                : ""
+                ? `<img src="${item.imagens[0]}" alt="${item.nome}" class="menu-item-image" onclick="(${this.abrirCarrosselCallback})('${JSON.stringify(item.imagens).replace(/'/g, "\\'")}')">`
+                : `<div class="menu-item-no-image">Sem Imagem</div>`
             }
-            <span>${item.nome} - R$ ${item.precoUnitario.toFixed(2)}</span>
+            <span>${item.nome} - R$ ${(item.precoUnitario || 0).toFixed(2)}</span>
             <button onclick="(${this.adicionarCallback})('${item.nome}')">
               Adicionar
             </button>
