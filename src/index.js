932969b0feceb0b7b997b87b3f52ea3062a0d1da
LogicMaster <traderrafa02@gmail.com>
2025-03-04 16:40:08 -0300

    uplogindex.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import { MesaInfo } from "./components/MesaInfo.js";
 import { Menu } from "./components/Menu.js";
 import { Pedido } from "./components/Pedido.js";
 import { enviarPedido } from "./services/api.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import {
   getDatabase,
   ref,
   onValue,
-  getApps,
+  
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
