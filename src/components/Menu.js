3a61ae3b5f61f5259ea019cec71232cd6bed9a83
LogicMaster <traderrafa02@gmail.com>
2025-03-04 16:35:11 -0300

    lognemMenu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,17 +2,20 @@ export class Menu {
   constructor(itens, adicionarCallback, abrirCarrosselCallback) {
     this.itens = itens;
     this.adicionarCallback = adicionarCallback;
-    this.abrirCarrosselCallback = abrirCarrosselCallback; // Novo callback para o carrossel
+    this.abrirCarrosselCallback = abrirCarrosselCallback;
   }
 
   render() {
     return this.itens
-      .map(
-        (item) => `
+      .map((item) => {
+        console.log("Item sendo renderizado:", item);
+        const imagensJson = JSON.stringify(item.imagens || []).replace(/'/g, "\\'");
+        console.log("String JSON para carrossel:", imagensJson);
+        return `
           <div class="menu-item">
             ${
               item.imagens && item.imagens.length > 0
-                ? `<img src="${item.imagens[0]}" alt="${item.nome}" class="menu-item-image" onclick="(${this.abrirCarrosselCallback})('${JSON.stringify(item.imagens).replace(/'/g, "\\'")}')">`
+                ? `<img src="${item.imagens[0]}" alt="${item.nome}" class="menu-item-image" onclick="(${this.abrirCarrosselCallback})('${imagensJson}')">`
                 : `<div class="menu-item-no-image">Sem Imagem</div>`
             }
             <span>${item.nome} - R$ ${(item.precoUnitario || 0).toFixed(2)}</span>
@@ -20,8 +23,8 @@ export class Menu {
               Adicionar
             </button>
           </div>
-        `
-      )
+        `;
+      })
       .join("");
   }
 }
